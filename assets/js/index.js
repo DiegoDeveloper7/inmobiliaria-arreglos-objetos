47555f23b9d1f075b70b40c1d131776f78d4dbce
DiegoDeveloper7 <diegoag8@gmail.com>
2025-05-18 16:24:52 -0400

    4. logica de arreglo y bucle js para crear propiedadesVenta.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,8 @@
 
 const departamentos = [
+ 
+    // 1ra tarjeta - index  
+
     {
       src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
       nombre: 'Apartamento de lujo en zona exclusiva',
@@ -11,7 +14,7 @@ const departamentos = [
       smoke: false,
       pets: false
     },
-    // Puedes agregar más objetos aquí
+    // 2da tarjeta - index
 
     {
         src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
@@ -19,13 +22,67 @@ const departamentos = [
         descripcion: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
         ubicacion: '789 Mountain Road, Summit Peaks, CA 23456',
         habitaciones: '2 Habitaciones |',
-        baños: '1 Baños',
+        baños: '1 Baño',
         costo: 1200,
         smoke: true,
         pets: true
       },
 
+    // 3ra tarjeta - index 
 
+      {
+        src: 'https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg',
+        nombre: 'Penthouse de lujo con terraza panorámica',
+        descripcion: 'Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares',
+        ubicacion: '567 Skyline Avenue,  Skyview City, CA 56789',
+        habitaciones: '3 Habitaciones |',
+        baños: '3 Baños',
+        costo: 4500,
+        smoke: false,
+        pets: true
+      },
+
+      // 4ta tarjeta - out index 
+
+      {
+        src: './assets/imgs/casa1.jpg',
+        nombre: 'Casa en Vitacura',
+        descripcion: 'Una incrible casa familiar en un sector privilegiado de la ciudad de Santigao.',
+        ubicacion: 'Avenida vitacura, Oriente 406',
+        habitaciones: '5 Habitaciones |',
+        baños: '4 Baños',
+        costo: 1000000,
+        smoke: true,
+        pets: true
+      },
+
+      // 5ta tarjeta - out index 
+
+      {
+        src: './assets/imgs/depa2.webp',
+        nombre: 'Departamento Las Condes',
+        descripcion: 'Gran departamento en reconcido sector de Las Condes.',
+        ubicacion: 'Avenida Padre Hurtado, Oriente 408',
+        habitaciones: '3 Habitaciones |',
+        baños: '2 Baños',
+        costo: 400000,
+        smoke: false,
+        pets: true
+      },
+
+      // 6ta tarjeta - out index 
+
+      {
+        src: './assets/imgs/depa3.jpeg',
+        nombre: 'Departamento Viña del Mar',
+        descripcion: 'Excelente departamento en la Ciudad de Viña del Mar y sus Gaviotas.',
+        ubicacion: 'Avenida del Deporte, Playa acapulco 407',
+        habitaciones: '3 Habitaciones |',
+        baños: '2 Baños',
+        costo: 350000,
+        smoke: true,
+        pets: false
+        },
 
   ];
   
